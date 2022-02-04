21ece1dd5a6e2012fa12aa49212a297707671245
LovroG05 <lovro.govekar@gmail.com>
2022-02-04 17:41:57 +0100

    i figured out embeds

--- src/main/kotlin/org/classcompanion/bot/commands/PingCommand.kt
+++ src/main/kotlin/org/classcompanion/bot/commands/PingCommand.kt
@@ -2,12 +2,12 @@ package org.classcompanion.bot.commands
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import org.classcompanion.bot.objects.SlashCommand
+import org.classcompanion.bot.utils.EmbedUtils
 
 class PingCommand : SlashCommand {
 	override fun executeCommand(event: SlashCommandInteractionEvent?) {
 		event?.deferReply(false)?.queue()
 		val interactionHook = event?.hook
-		interactionHook?.sendMessage("pong")?.queue()
+		interactionHook?.sendMessageEmbeds(EmbedUtils.basicTextEmbed("PONG", "pond", "I pong'd"))?.queue()
 	}
-
 }
