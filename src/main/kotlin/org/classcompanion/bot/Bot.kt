6d479952cb77bd2dbecd3edb7eb707a7554da57c
LovroG05 <lovro.govekar@gmail.com>
2022-02-10 19:31:59 +0100

    cleanup

--- src/main/kotlin/org/classcompanion/bot/Bot.kt
+++ src/main/kotlin/org/classcompanion/bot/Bot.kt
@@ -6,7 +6,6 @@ import net.dv8tion.jda.api.entities.Activity
 import net.dv8tion.jda.api.entities.TextChannel
 import net.dv8tion.jda.api.interactions.commands.build.Commands
 import net.dv8tion.jda.api.utils.ChunkingFilter
-import org.classcompanion.bot.commands.PingCommand
 import org.classcompanion.bot.commands.SetTextChannelCommand
 import org.classcompanion.bot.consumes.MessageConsume
 import org.classcompanion.bot.managers.SlashCommandManager
@@ -21,23 +20,19 @@ class Bot(token: String, guild: String, rabbitMqIP: String) {
 		val bot = BotLib(rabbitMqIP, guild)
 		builder.setActivity(Activity.playing("Portal 2: Education"))
 		builder.setChunkingFilter(ChunkingFilter.ALL)
-		builder.addEventListeners(/*messagesManager, */slashCommandManager)
+		builder.addEventListeners(slashCommandManager)
 
 		jda = builder.build()
 		jda.awaitReady()
 
 		jda.updateCommands().addCommands(
-			Commands.slash("ping", "ping"),
 			Commands.slash("setdefaultchannel", "Set the default text channel")
 		)
 
-		slashCommandManager.registerCommand("ping", PingCommand())
 		slashCommandManager.registerCommand("setdefaultchannel", SetTextChannelCommand(bot))
-
 
 		val defaultChannel: TextChannel? = jda.getTextChannelById(753495220888535080)
 		bot.setConsume(MessageConsume(defaultChannel!!, bot))
-		//bot.setConsume(ToMessageConsume(defaultChannel!!))
 	}
 }
 
