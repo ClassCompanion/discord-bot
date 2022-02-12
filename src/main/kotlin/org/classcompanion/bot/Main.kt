a37a918d64d58074e4e009621e857f3058b5dff8
LovroG05 <lovro.govekar@gmail.com>
2022-02-12 19:20:46 +0100

    changed from .env to env vars

--- src/main/kotlin/org/classcompanion/bot/Main.kt
+++ src/main/kotlin/org/classcompanion/bot/Main.kt
@@ -4,8 +4,8 @@ import io.github.cdimascio.dotenv.Dotenv
 
 fun main(args: Array<String>) {
 	val dotenv: Dotenv = Dotenv.load()
-	val token = dotenv["TOKEN"]
-	val guild = dotenv["GUILD"]
-	val rabbitMqIP = dotenv["RABBITMQ-IP"]
+	val token = System.getenv("TOKEN")
+	val guild = System.getenv("GUILD")
+	val rabbitMqIP = System.getenv("RABBITMQ-IP")
 	Bot(token!!, guild!!, rabbitMqIP!!)
 }
