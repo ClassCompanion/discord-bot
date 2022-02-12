0e7a63ccc7fdb995433fb5d7069a394e130f0043
LovroG05 <lovro.govekar@gmail.com>
2022-02-12 20:33:04 +0100

    dockerfile

--- src/main/kotlin/org/classcompanion/bot/Main.kt
+++ src/main/kotlin/org/classcompanion/bot/Main.kt
@@ -1,9 +1,9 @@
 package org.classcompanion.bot
 
-import io.github.cdimascio.dotenv.Dotenv
+//import io.github.cdimascio.dotenv.Dotenv
 
 fun main(args: Array<String>) {
-	val dotenv: Dotenv = Dotenv.load()
+	//val dotenv: Dotenv = Dotenv.load()
 	val token = System.getenv("TOKEN")
 	val guild = System.getenv("GUILD")
 	val rabbitMqIP = System.getenv("RABBITMQ-IP")
