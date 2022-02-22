5ef98b4e9f824ae4fe68ce0b36ec8e8df94d2c31
LovroG05 <lovro.govekar@gmail.com>
2022-02-22 10:55:16 +0100

    it fokin werks

--- src/main/kotlin/org/classcompanion/bot/consumes/AssessmentConsume.kt
+++ src/main/kotlin/org/classcompanion/bot/consumes/AssessmentConsume.kt
@@ -1,5 +1,6 @@
 package org.classcompanion.bot.consumes
 
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.rabbitmq.client.Delivery
 import net.dv8tion.jda.api.JDA
@@ -13,16 +14,14 @@ class AssessmentConsume(private val bot: BotLib, private val jda: JDA, private v
     override fun execute(delivery: Delivery, charset: Charset) {
         println(String(delivery.body, charset))
         val mapper = jacksonObjectMapper()
-        val q = mapper.readValue<Assessment>(String(delivery.body, charset), Assessment::class.java)
+        val q: Assessment = mapper.readValue<Assessment>(String(delivery.body, charset), Assessment::class.java)
         println(q.title)
-        if (q.messageId == null) {
+        if (q.message_id == null) {
             jda.getTextChannelById(channel)?.sendMessageEmbeds(EmbedUtils.datesEmbed(q))
                 ?.queue()
         } else {
-            println(q.messageId)
-            jda.getTextChannelById(channel)?.editMessageEmbedsById(q.messageId, EmbedUtils.datesEmbed(q))?.queue()
+            println(q.message_id)
+            jda.getTextChannelById(channel)?.editMessageEmbedsById(q.message_id!!, EmbedUtils.datesEmbed(q))?.queue()
         }
-
-        // a
     }
 }
