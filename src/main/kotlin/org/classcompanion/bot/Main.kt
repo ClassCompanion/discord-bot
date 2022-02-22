aa7f61bebbb4f0440262ea56a1b933766a53a939
LovroG05 <lovro.govekar@gmail.com>
2022-02-22 20:56:07 +0100

    preparing for push to master

--- src/main/kotlin/org/classcompanion/bot/Main.kt
+++ src/main/kotlin/org/classcompanion/bot/Main.kt
@@ -1,22 +1,22 @@
 package org.classcompanion.bot
 
-import io.github.cdimascio.dotenv.Dotenv
+//import io.github.cdimascio.dotenv.Dotenv
 
 fun main(args: Array<String>) {
 	// for production, use ENV variables when running
-	/*val token = System.getenv("TOKEN")
+	val token = System.getenv("TOKEN")
 	val guild = System.getenv("GUILD")
 	val rabbitMqIP = System.getenv("RABBITMQ-IP")
 	val rabbitusername = System.getenv("RABBITMQ-USERNAME")
 	val rabbitpassword = System.getenv("RABBITMQ-PASSWD")
-	Bot(token!!, guild!!, rabbitMqIP!!, System.getenv("CHANNEL")!!)*/
+	Bot(token!!, guild!!, rabbitMqIP!!, rabbitusername!!, rabbitpassword!!, System.getenv("CHANNEL")!!)
 
 	// for testing using a .env file
-	val dotenv: Dotenv = Dotenv.load()
+	/*val dotenv: Dotenv = Dotenv.load()
 	val token = dotenv["TOKEN"]
 	val guild = dotenv["GUILD"]
 	val rabbitMqIP = dotenv["RABBITMQ-IP"]
 	val rabbitusername = dotenv["RABBITMQ-USERNAME"]
 	val rabbitpassword = dotenv["RABBITMQ-PASSWD"]
-	Bot(token!!, guild!!, rabbitMqIP!!, rabbitusername!!, rabbitpassword!!, dotenv["CHANNEL"]!!)
+	Bot(token!!, guild!!, rabbitMqIP!!, rabbitusername!!, rabbitpassword!!, dotenv["CHANNEL"]!!)*/
 }
